804c0f1a5f276e7a1a9bc31d7a480174cf4ae40f
ParisHub <hagen.schroeder@outlook.com>
2026-01-29 08:05:05 +0000

    learning more #2

--- main.c
+++ main.c
@@ -1,12 +1,41 @@
 #include <stdio.h>
+#include <string.h>
 
 int helloc(void) {
     printf("Hello, C.\n");
     return 0;
 }
 
-int main(void) {
-    helloc();
+int createHtml(void) {
+    FILE *file = fopen("main.html", "w");
+    if (file == NULL) {
+        printf("Failed to create file\n");
+        return 1;
+    }
+    fprintf(file, 
+        "<!DOCTYPE html>\n"
+        "<html lang=\"en\">\n"
+        "<head>\n"
+        "    <meta charset=\"UTF-8\">\n"
+        "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
+        "    <title>HelloKitty</title>\n"
+        "</head>\n"
+        "<body>\n"
+        "    <h1>Welcome</h1>\n"
+        "</body>\n"
+        "</html>\n"
+    );
+}
+
+int main(int argc, char* argv[]) {
+    if (argc == 2) {
+        if (strcmp(argv[1], "helloc") == 0) {
+            helloc();
+        }
+        if (strcmp(argv[1], "chtml") == 0) {
+            createHtml();
+        }
+    }
     return 0;
 }
 
